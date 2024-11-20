8e1efd1faebe92bffe51b713a901a378602abe30
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2024-11-20 23:24:42 +0100

    init

---
+++ src/main/java/com/mehmedmaljoki/testingspringbootapplications/TestingSpringBootApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.mehmedmaljoki.testingspringbootapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSpringBootApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingSpringBootApplicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehmedmaljoki/testingspringbootapplications/TestingSpringBootApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmedmaljoki.testingspringbootapplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSpringBootApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
