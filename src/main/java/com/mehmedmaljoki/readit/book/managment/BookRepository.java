0a9877fd699b6aea86b02c4d741f47b09a72ec6a
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2024-12-06 00:10:59 +0100

    add book repo

---
+++ src/main/java/com/mehmedmaljoki/readit/book/managment/BookRepository.java
@@ -0,0 +1,7 @@
+package com.mehmedmaljoki.readit.book.managment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+  Book findByIsbn(String isbn);
+}
