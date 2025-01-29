1284dfedd932c01f21439be26953ca7080ae1441
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2025-01-29 15:52:53 +0100

    add first BookSynchronizationListener test

--- src/test/java/com/mehmedmaljoki/readit/book/managment/BookSynchronizationListenerTest.java
+++ src/test/java/com/mehmedmaljoki/readit/book/managment/BookSynchronizationListenerTest.java
@@ -1,14 +1,34 @@
 package com.mehmedmaljoki.readit.book.managment;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyNoInteractions;
 
+
+@ExtendWith(MockitoExtension.class)
 class BookSynchronizationListenerTest {
+
+
+  @Mock
+  private BookRepository bookRepository;
+
+  @Mock
+  private OpenLibraryApiClient openLibraryApiClient;
+
+  @InjectMocks
+  private BookSynchronizationListener cut; // class under test
 
   @Test
   void shouldRejectBookWhenIsbnIsMalformed() {
 
+    var bookSynchronization = new BookSynchronization("42");
+    cut.consumeBookUpdates(bookSynchronization);
+
+    verifyNoInteractions(openLibraryApiClient, bookRepository);
   }
 
   @Test
