e1d713132077a772b648c4d33cbfffcb82edf16f
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2025-01-30 17:10:15 +0100

    intro to JsonPath

--- src/test/java/com/mehmedmaljoki/readit/JsonTest.java
+++ src/test/java/com/mehmedmaljoki/readit/JsonTest.java
@@ -1,6 +1,8 @@
 package com.mehmedmaljoki.readit;
 
+import com.jayway.jsonpath.JsonPath;
 import org.json.JSONException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
@@ -12,5 +14,12 @@ public class JsonTest {
   void testWithJSONAssert() throws JSONException {
     var result = "{\"name\": \"duke\", \"age\":\"42\", \"hobbies\": [\"java\", \"junit\", \"spring\"]}";
     assertEquals("{\"name\": \"duke\", \"age\":\"42\", \"hobbies\": [\"java\", \"junit\", \"spring\"]}", result, true);
+  }
+
+  @Test
+  void testWithJsonPath() {
+    var result = "{\"name\": \"duke\", \"age\":\"42\", \"hobbies\": [\"java\", \"junit\", \"spring\"]}";
+    Assertions.assertEquals(3, JsonPath.parse(result).read("$.hobbies.length()", Long.class));
+    Assertions.assertEquals("duke", JsonPath.parse(result).read("$.name"));
   }
 }
